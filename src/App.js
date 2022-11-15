13a4baec3548dd196ab2d5143871252cb180f1c3
ChanduMediboina <chandumediboina95@gmail.com>
2022-11-15 06:54:27 +0000

    assignment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Route, Switch} from 'react-router-dom'
+import {Route, BrowserRouter, Switch} from 'react-router-dom'
 
 import YourTab from './components/YourTab'
 
@@ -11,12 +11,14 @@ import NotFound from './components/NotFound'
 import './App.css'
 
 const App = () => (
-  <Switch>
-    <Route exact path="/" component={AllTab} />
-    <Route exact path="/your" component={YourTab} />
-    <Route exact path="/blocked" component={BlockedTab} />
-    <NotFound component={NotFound} />
-  </Switch>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={AllTab} />
+      <Route exact path="/your" component={YourTab} />
+      <Route exact path="/blocked" component={BlockedTab} />
+      <NotFound component={NotFound} />
+    </Switch>
+  </BrowserRouter>
 )
 
 export default App
